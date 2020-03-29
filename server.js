c2ad087ef1948ceba7b9710d1ca3becd9bd90a0a
AnthonyAmaker <tamaker@gmail.com>
2020-03-29 13:47:22 -0400

    removing try catch for simplicity of this example

--- server.js
+++ server.js
@@ -5,39 +5,25 @@ var myObj = {"time": "111111111"}
 
 function doFirstThing(){
     return new Promise(function(resolve, reject){
-        try {
-            fs.readFile('./file1.txt', function(err, data){
-                if (!err){
-                    resolve(data.toString())
-                } else {
-                    reject('problem with file 1')
-                }
-            })
-        } catch(e){
-            console.log('error!');
-            console.log(e);
-        } finally {
-            console.log('file 1 finished')
-        }
+        fs.readFile('./file1.txt', function(err, data){
+            if (!err){
+                resolve(data.toString())
+            } else {
+                reject('problem with file 1')
+            }
+        })
     })
 }
 
 function doSecondThing(){
     return new Promise(function(resolve, reject){
-        try {
-            fs.readFile('./file2.txt', function(err, data){
-                if (!err){
-                    resolve(data.toString())
-                } else {
-                    reject('problem with file 2')
-                }
-            })
-        } catch(e){
-            console.log('error!');
-            console.log(e);
-        } finally {
-            console.log('file 2 finished')
-        }
+        fs.readFile('./file2.txt', function(err, data){
+            if (!err){
+                resolve(data.toString())
+            } else {
+                reject('problem with file 2')
+            }
+        })
     })
 }
 
