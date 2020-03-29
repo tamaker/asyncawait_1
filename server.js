a2cc2f4b979774ab24b40e2d69c353476beb8b4b
AnthonyAmaker <tamaker@gmail.com>
2020-03-29 13:43:08 -0400

    added try catch for error handling

--- server.js
+++ server.js
@@ -5,21 +5,39 @@ var myObj = {"time": "111111111"}
 
 function doFirstThing(){
     return new Promise(function(resolve, reject){
-        fs.readFile('./file1.txt', function(err, data){
-            if (!err){
-                resolve(data.toString())
-            }
-        })
+        try {
+            fs.readFile('./file1.txt', function(err, data){
+                if (!err){
+                    resolve(data.toString())
+                } else {
+                    reject('problem with file 1')
+                }
+            })
+        } catch(e){
+            console.log('error!');
+            console.log(e);
+        } finally {
+            console.log('file 1 finished')
+        }
     })
 }
 
 function doSecondThing(){
     return new Promise(function(resolve, reject){
-        fs.readFile('./file2.txt', function(err, data){
-            if (!err){
-                resolve(data.toString())
-            }
-        })
+        try {
+            fs.readFile('./file2.txt', function(err, data){
+                if (!err){
+                    resolve(data.toString())
+                } else {
+                    reject('problem with file 2')
+                }
+            })
+        } catch(e){
+            console.log('error!');
+            console.log(e);
+        } finally {
+            console.log('file 2 finished')
+        }
     })
 }
 
