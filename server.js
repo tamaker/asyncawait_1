4be1d0e60357d960e4d8fdfa23f910027c9f08e7
AnthonyAmaker <tamaker@gmail.com>
2020-03-29 13:36:42 -0400

    works by chaining, but can be better

--- server.js
+++ server.js
@@ -2,22 +2,32 @@ var fs = require('fs');
 
 var myObj = {"time": "111111111"}
 
-fs.readFile('./file1.txt', function(err, data){
-    if (!err){
-        myObj['data1'] = data.toString();
-        console.log(data.toString())
-    } else {
-        console.log(err)
-    }
+
+function doFirstThing(){
+    return new Promise(function(resolve, reject){
+        fs.readFile('./file1.txt', function(err, data){
+            if (!err){
+                resolve(data.toString())
+            }
+        })
+    })
+}
+
+function doSecondThing(){
+    return new Promise(function(resolve, reject){
+        fs.readFile('./file2.txt', function(err, data){
+            if (!err){
+                resolve(data.toString())
+            }
+        })
+    })
+}
+
+doFirstThing().then(function(data){
+    myObj['data1'] = data;
+    doSecondThing().then(function(data2){
+        myObj['data2'] = data2;
+        console.log(myObj)
+    })
 })
 
-fs.readFile('./file2.txt', function(err, data){
-    if (!err){
-        myObj['data2'] = data.toString();
-        console.log(data.toString())
-    } else {
-        console.log(err)
-    }
-})
-
-console.log(myObj);
